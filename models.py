8492983780c6352f60c9724d0c55b85c7c5d3ee0
Chrisando Ryan <chrisando.siahaan@gmail.com>
2021-08-23 18:26:09 +0700

    update training script for final exam Big Data Analytics

--- models.py
+++ models.py
@@ -3,17 +3,17 @@ from utils import printfo
 # Model 1: TF-IDF + Logistic Regression
 # Reference: https://towardsdatascience.com/sentiment-analysis-with-pyspark-bc8e83f80c35
 def train_lr(dataset):
-    train_set, val_set, test_set = dataset
+    train_set, val_set = dataset
 
     from pyspark.ml.feature import HashingTF, IDF, Tokenizer
     from pyspark.ml.feature import StringIndexer
     from pyspark.ml import Pipeline
 
     printfo("Executing tokenizer and TF-IDF...")
-    tokenizer = Tokenizer(inputCol="tweet", outputCol="words")
+    tokenizer = Tokenizer(inputCol="text", outputCol="words")
     hashtf = HashingTF(numFeatures=2**16, inputCol="words", outputCol='tf')
     idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
-    label_stringIdx = StringIndexer(inputCol="label", outputCol="label_index")
+    label_stringIdx = StringIndexer(inputCol="target", outputCol="label")
 
     printfo("Creating pipeline...")
     pipeline = Pipeline(stages=[tokenizer, hashtf, idf, label_stringIdx])
@@ -27,7 +27,7 @@ def train_lr(dataset):
     printfo("Start training with Logistic Regression...")
     from pyspark.ml.classification import LogisticRegression
     lr = LogisticRegression(maxIter=100)
-    lrModel = lr.fit(train_df)
+    lrModel = lr.fit(train_df, )
     prediction_lr = lrModel.transform(val_df)
 
     printfo("Displaying prediction sample...")
@@ -36,30 +36,40 @@ def train_lr(dataset):
 
     printfo("Starting evaluation...")
     from pyspark.ml.evaluation import BinaryClassificationEvaluator
-    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction", labelCol="label")
 
-    printfo("[Logistic Regression] Accuracy: ", evaluator.evaluate(prediction_lr))
+    n_correct = prediction_lr.filter(prediction_lr.label == prediction_lr.prediction).count()
+    printfo("[Logistic Regression] Correct prediction: %s / %s" % (n_correct, val_set.count()))
+    accuracy = n_correct / float(val_set.count())
+    printfo("[Logistic Regression] Accuracy: ", accuracy)
+
+    from sklearn.metrics import classification_report, confusion_matrix
+    y_true = prediction_lr.select(['label']).collect()
+    y_pred = prediction_lr.select(['prediction']).collect()
+    print(classification_report(y_true, y_pred))
+
     return
 
 
 # Model 2: TF-IDF + Naive Bayes
 # Reference: https://github.com/shikha720/Sentiment-Analysis-and-Text-Classification-Using-PySpark
 def train_naive(dataset):
-    train_set, val_set, test_set = dataset
+    train_set, val_set = dataset
 
     from pyspark.ml.feature import HashingTF, IDF, Tokenizer
     from pyspark.ml.feature import StringIndexer
     from pyspark.ml import Pipeline
 
     printfo("Executing tokenizer and TF-IDF...")
-    tokenizer = Tokenizer(inputCol="tweet", outputCol="words")
+    tokenizer = Tokenizer(inputCol="text", outputCol="words")
     hashtf = HashingTF(numFeatures=2**16, inputCol="words", outputCol='tf')
     idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5) #minDocFreq: remove sparse terms
+    label_stringIdx = StringIndexer(inputCol="target", outputCol="label")
 
     printfo("Start training with NaiveBayes...")
     from pyspark.ml.classification import NaiveBayes
     nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
-    pipeline_nb = Pipeline(stages=[tokenizer, hashtf, idf, nb])
+    pipeline_nb = Pipeline(stages=[tokenizer, hashtf, idf, label_stringIdx, nb])
     nbModel = pipeline_nb.fit(train_set)
     prediction_nb = nbModel.transform(val_set)
 
@@ -69,7 +79,16 @@ def train_naive(dataset):
 
     printfo("Starting evaluation...")
     from pyspark.ml.evaluation import BinaryClassificationEvaluator
-    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", labelCol="label")
 
     printfo("Starting evaluation...")
-    printfo("[Naive Bayes] Accuracy: ", evaluator.evaluate(prediction_nb))
+    n_correct = prediction_nb.filter(prediction_nb.label == prediction_nb.prediction).count()
+    printfo("[Naive Bayes] Correct prediction: %s / %s" % (n_correct, val_set.count()))
+    accuracy = n_correct / float(val_set.count())
+    printfo("[Naive Bayes] Accuracy: ", accuracy)
+
+    from sklearn.metrics import classification_report, confusion_matrix
+    y_true = prediction_nb.select(['label']).collect()
+    y_pred = prediction_nb.select(['prediction']).collect()
+    print(classification_report(y_true, y_pred))
+
